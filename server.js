9a25a44f82084ffdd6b9871c2b2b555feb6df5d7
mantzinos <mantzinos@hotmail.com>
2021-05-10 19:18:17 +0300

    sex

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api", require("./routes/api/user"));
 app.use("/api/vehicle", require("./routes/api/vehicle"));
