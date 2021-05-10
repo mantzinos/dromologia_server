20d86bf3e84403ca1b7a6225caf76e8336a42b75
mantzinos <mantzinos@hotmail.com>
2021-05-10 18:24:27 +0300

    secret

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const config = require("config");
 const mongoose = require("mongoose");
-const db = config.get("mongoURI");
+const db = config.get("secrets.MONGO_URI");
 
 const connectDB = async () => {
   try {
